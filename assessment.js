e5db855199cc6519b54b18eb39c045ac82f4b372
sorayyy <85696917+sorayyy@users.noreply.github.com>
2021-07-02 21:46:38 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -5,16 +5,16 @@ const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
 /**
- * 指定した要素の子どもを全て除去する
+ * 指定した要素の子供を全て削除する
  * @param {HTMLElement} element HTMLの要素
  */
 function removeAllChildren(element) {
-  while (element.firstChild) {
-    // 子どもの要素があるかぎり除去
+   while (element.firstChild) {
+    //子どもの要素があるかぎり削除
     element.removeChild(element.firstChild);
-  }
+   }
 }
-
+ 
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
   if (userName.length === 0) {
@@ -33,27 +33,24 @@ assessmentButton.onclick = () => {
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-  // ツイートエリアの作成
+  // TODO ツイートエリアの作成
   removeAllChildren(tweetDivided);
   const anchor = document.createElement('a');
   const hrefValue =
-    'https://twitter.com/intent/tweet?button_hashtag=' +
-    encodeURIComponent('あなたのいいところ') +
-    '&ref_src=twsrc%5Etfw';
+  'https://twitter.com/intent/tweet?button_hashtag=' +
+  encodeURIComponent('あなたのいいところ') +
+  '&ref_src=twsrc%5Etfw';
+  
   anchor.setAttribute('href', hrefValue);
   anchor.className = 'twitter-hashtag-button';
   anchor.setAttribute('data-text', result);
   anchor.innerText = 'Tweet #あなたのいいところ';
+  
   tweetDivided.appendChild(anchor);
-
-  // widgets.js の設定
-  const script = document.createElement('script');
-  script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
-  tweetDivided.appendChild(script);
 };
 
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
+  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
   '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
@@ -78,13 +75,13 @@ const answers = [
  */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
-  let sumOfcharCode = 0;
+  let sumOfCharCode = 0;
   for (let i = 0; i < userName.length; i++) {
-    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
   }
 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfcharCode % answers.length;
+  const index = sumOfCharCode % answers.length;
   let result = answers[index];
 
   result = result.replaceAll('{userName}', userName);
